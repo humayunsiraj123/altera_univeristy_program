5becee482ecd8a2f4c4bf042a0ddd9d28e4e5fb3
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-10-27 19:58:39 +0500

    Update and rename mux_N_to_M.sv to mux_M_wide_N_to_1.sv

--- LAB_1/mux_M_wide_N_to_1.sv
+++ LAB_1/mux_M_wide_N_to_1.sv
@@ -1,4 +1,4 @@
-module mux_N_to_M#(parameter NUM_SEL = 3,WIDTH = 1)(
+module mux_M_wide_N_to_1#(parameter NUM_SEL = 3,WIDTH = 1)(
   input [2**NUM_SEL-1:0]in[WIDTH-1:0],
   input [NUM_SEL-1:0]sel,
   output  logic  [WIDTH-1:0]out
